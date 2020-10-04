fd9caa2675bdb81027375fa1b52d8fa5c57c1521
Arianaomi <arianaomi.lp@gmail.com>
2020-10-03 20:43:52 -0500

    :tada: Initial commit
    Initial commit

---
+++ index.js
@@ -0,0 +1,135 @@
+
+
+/*input*/
+/*
+[
+    {
+        version:"1",
+        name:"Lorem ipsum dolor sit.",
+        description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus, eligendi."
+    },
+    {
+        version:"1.1",
+        name:"Lorem ipsum dolor sit.",
+        description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus, eligendi."
+    },
+    {
+        version:"1.2",
+        name:"Lorem ipsum dolor sit.",
+        description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus, eligendi."
+    },
+    {
+        version:"1.3",
+        name:"Lorem ipsum dolor sit.",
+        description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus, eligendi."
+    },
+    {
+        version:"2",
+        name:"Lorem ipsum dolor sit.",
+        description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus, eligendi."
+    },
+    {
+        version:"2.1",
+        name:"Lorem ipsum dolor sit.",
+        description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus, eligendi."
+    },{
+        version:"2.1.1",
+        name:"Lorem ipsum dolor sit.",
+        description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus, eligendi."
+    },
+    {
+        version:"2.1.2",
+        name:"Lorem ipsum dolor sit.",
+        description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus, eligendi."
+    },
+    {
+        version:"3",
+        name:"Lorem ipsum dolor sit.",
+        description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus, eligendi."
+    },
+    {
+        version:"3.1",
+        name:"Lorem ipsum dolor sit.",
+        description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus, eligendi."
+    },
+    {
+        version:"3.2",
+        name:"Lorem ipsum dolor sit.",
+        description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus, eligendi."
+    },
+    {
+        version:"3.3",
+        name:"Lorem ipsum dolor sit.",
+        description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus, eligendi."
+    }
+]
+*/
+/*output*/
+/*
+[
+    {
+        version:"1",
+        name:"Lorem ipsum dolor sit.",
+        description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus, eligendi.",
+        subVersions:[
+            {
+                version:"1.1",
+                name:"Lorem ipsum dolor sit.",
+                description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus, eligendi."
+            },
+            {
+                version:"1.2",
+                name:"Lorem ipsum dolor sit.",
+                description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus, eligendi."
+            },
+            {
+                version:"1.3",
+                name:"Lorem ipsum dolor sit.",
+                description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus, eligendi."
+            }
+        ]
+    },
+    {
+        version:"2",
+        name:"Lorem ipsum dolor sit.",
+        description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus, eligendi.",
+        subVersions:[
+            {
+                version:"2.1",
+                name:"Lorem ipsum dolor sit.",
+                description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus, eligendi."
+            },{
+                version:"2.1.1",
+                name:"Lorem ipsum dolor sit.",
+                description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus, eligendi."
+            },
+            {
+                version:"2.1.2",
+                name:"Lorem ipsum dolor sit.",
+                description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus, eligendi."
+            }
+        ]
+    },
+    {
+        version:"3",
+        name:"Lorem ipsum dolor sit.",
+        description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus, eligendi.",
+        subVersions:[
+            {
+                version:"3.1",
+                name:"Lorem ipsum dolor sit.",
+                description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus, eligendi."
+            },
+            {
+                version:"3.2",
+                name:"Lorem ipsum dolor sit.",
+                description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus, eligendi."
+            },
+            {
+                version:"3.3",
+                name:"Lorem ipsum dolor sit.",
+                description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus, eligendi."
+            }
+        ]
+    }
+]
